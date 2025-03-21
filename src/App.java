7981d114b7849e228d48976fd9c81efca61a8e6a
inouye165 <inouye165@gmail.com>
2025-03-21 11:58:15 -0700

    feat: Implement and test Part3 string manipulation methods
    This commit adds the Part3 class with 'twoOccurrences' and 'lastPart' methods, along with comprehensive testing in the 'testing' method. It also updates App.java to include Part3 in the menu and refactors Part2 to call the correct method.

--- src/App.java
+++ src/App.java
@@ -1,11 +1,52 @@
+// App.java
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Output from Part1:");
-        Part1 part1 = new Part1();      // Create an instance of Part1
-        part1.testSimpleGene();          // Call the test method from Part1
+        Scanner scanner = new Scanner(System.in);
+        int choice;
 
-        System.out.println("Output from Part2:");
-        Part2 part2 = new Part2();      // Create an instance of Part2
-        part2.testSimpleGene();         // Call the test method from Part2
+        do {
+            System.out.println("Choose which part to run:");
+            System.out.println("1. Part1");
+            System.out.println("2. Part2");
+            System.out.println("3. Part3");
+            System.out.println("4. Part4");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+
+            choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Running Part1:");
+                    Part1 part1 = new Part1();
+                    part1.testSimpleGene();
+                    break;
+                case 2:
+                    System.out.println("Running Part2:");
+                    Part2 part2 = new Part2();
+                    part2.testSimpleGene(); // Corrected to call testSimpleGene()
+                    break;
+                case 3:
+                    System.out.println("Running Part3:");
+                    Part3 part3 = new Part3();
+                    part3.testing();
+                    break;
+                case 4:
+                    System.out.println("Running Part4:");
+                    Part4 part4 = new Part4();
+                    part4.runPart4();
+                    break;
+                case 0:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice != 0);
+
+        scanner.close();
     }
 }
---
+++ src/Part4.java
@@ -0,0 +1,6 @@
+// Part4.java
+public class Part4 {
+    public void runPart4() {
+        System.out.println("Part4 placeholder");
+    }
+}
